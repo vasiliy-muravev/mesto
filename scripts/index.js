76c4c64014be1a7d7be5cc90cff8642ddecaa373
Vasily Muraviev <muravievvv@develop.mos.ru>
2022-03-29 23:00:33 +0300

    first review fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,49 +1,39 @@
+/* Попап */
 const popupElement = document.querySelector('.popup');
 const redactButton = document.querySelector('.info__redact-button');
 const closeButton = popupElement.querySelector('.popup__close-button');
 const ESC_KEY = "Escape";
 
-/* Открытие попапа */
-function openPopup() {
-    popupElement.classList.add('popup_opened');
-    document.addEventListener('keyup', onDocumentKeyUp);
-}
-
-/* Закрытие попапа */
-function closePopup() {
-    popupElement.classList.remove('popup_opened');
-    document.removeEventListener('keyup', onDocumentKeyUp);
-}
-
-/* Обработчик клавиши Esc */
-function onDocumentKeyUp(event) {
-    if (event.key === ESC_KEY) {
-        closePopup();
-    }
-}
-
-/* Открыть попап по кнопке редактировать */
-redactButton.addEventListener('click', openPopup);
-
-/* Закрыть попап на крестик */
-closeButton.addEventListener('click', closePopup);
-
-/* Кнопка сохранить */
-let formSubmit = popupElement.querySelector('.popup__form-submit-btn');
+/* Форма */
+const form = popupElement.querySelector('.popup__form');
 /* Поля формы */
 let nameInput = popupElement.querySelector('.popup__form-name');
 let jobInput = popupElement.querySelector('.popup__form-profession');
 let infoTitle = document.querySelector('.info__title');
 let infoSubtitle = document.querySelector('.info__subtitle');
 
-
 /* Обработчик «отправки» формы */
 function formSubmitHandler(event) {
     event.preventDefault();
     infoTitle.textContent = nameInput.value;
     infoSubtitle.textContent = jobInput.value;
-    closePopup();
+    popupToggle();
 }
 
+/* Открытие|закрытие попапа */
+function popupToggle() {
+    if (!popupElement.classList.contains('popup_opened')) {
+        nameInput.value = infoTitle.innerHTML;
+        jobInput.value = infoSubtitle.innerHTML;
+    }
+    popupElement.classList.toggle('popup_opened');
+}
+
+/* Открыть попап по кнопке редактировать */
+redactButton.addEventListener('click', popupToggle);
+
+/* Закрыть попап на крестик */
+closeButton.addEventListener('click', popupToggle);
+
 /* Отправка формы */
-formSubmit.addEventListener('click', formSubmitHandler);
+form.addEventListener('submit', formSubmitHandler);
