539dceffbde13e4c975abc2d80062c336e714588
Vasily Muraviev <muravievvv@develop.mos.ru>
2022-04-03 16:34:11 +0300

    Add popup card form

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,27 @@
 /* Попап */
 const popupElement = document.querySelector('.popup');
 const redactButton = document.querySelector('.info__redact-button');
-const closeButton = popupElement.querySelector('.popup__close-button');
+const closeProfileButton = popupElement.querySelector('.popup__close-button');
 
-/* Форма */
-const form = popupElement.querySelector('.popup__form');
-/* Поля формы */
+/* Элементы попапа формы профиля пользователя */
+const profileFormPopup = document.querySelector('#profileFormPopup');
 const nameInput = popupElement.querySelector('.popup__form-name');
 const jobInput = popupElement.querySelector('.popup__form-profession');
 const infoTitle = document.querySelector('.info__title');
 const infoSubtitle = document.querySelector('.info__subtitle');
+const profileForm = document.forms.profileForm;
 
+/* Элементы попапа формы добавления места */
+const placeFormPopup = document.querySelector('#placeFormPopup');
+const addPlaceButton = document.querySelector('.place__add-place-button');
+const closePlaceButton = placeFormPopup.querySelector('.popup__close-button');
+const placeForm = document.forms.placeForm;
+const placeNameInput = placeFormPopup.querySelector('.popup__form-name');
+const placeLinkInput = placeFormPopup.querySelector('.popup__form-profession');
+/* Элементы попапа с фотографиями */
+
+
+/* Стартовый набор мест */
 const initialCards = [
     {
         name: 'Архыз',
@@ -37,48 +48,96 @@ const initialCards = [
         link: './images/olkhon.jpg'
     }
 ];
-
-/* Шаблон карточки места */
+/* Добавить место используя шаблон карточки места */
 const addPlace = (name, link) => {
     const placeContainer = document.querySelector('.places');
     const placeTemplate = document.querySelector('#place-template').content;
     const placeElement = placeTemplate.querySelector('.place').cloneNode(true);
-    placeElement.querySelector('.place__image').src = link;
-    placeElement.querySelector('.place__image').alt = name;
-    placeElement.querySelector('.description__title').textContent = name;
+    placeElement.querySelector('.place__image').src = link !== '' ? link : './images/no-photo.jpg';
+    placeElement.querySelector('.place__image').alt = name !== '' ? name : '';
+    placeElement.querySelector('.description__title').textContent = name !== '' ? name : 'Нет названия';
     placeContainer.prepend(placeElement);
 };
-
-/* Шесть карточек «из коробки» */
+/* Добавляем шесть карточек «из коробки» */
 initialCards.forEach(function (item) {
     addPlace(item.name, item.link);
 });
 
 
-/* Обработчик «отправки» формы */
-function formSubmitHandler(event) {
+/* Обработчик «отправки» формы профиля пользователя */
+function formSubmitProfile(event) {
     event.preventDefault();
     infoTitle.textContent = nameInput.value;
     infoSubtitle.textContent = jobInput.value;
-    popupToggle();
+    popupToggle(profileFormPopup);
+}
+
+
+/* Обработчик «отправки» формы добавления места */
+function formSubmitPlace(event) {
+    event.preventDefault();
+    addPlace(placeNameInput.value, placeLinkInput.value);
+    placeNameInput.value = '';
+    placeLinkInput.value = '';
+    popupToggle(placeFormPopup);
 }
 
 /* Открытие|закрытие попапа */
-function popupToggle() {
-    if (!popupElement.classList.contains('popup_opened')) {
+function popupToggle(thisPopup) {
+    if (!thisPopup.classList.contains('popup_opened')) {
         nameInput.value = infoTitle.innerHTML;
         jobInput.value = infoSubtitle.innerHTML;
     }
-    popupElement.classList.toggle('popup_opened');
+    thisPopup.classList.toggle('popup_opened');
 }
 
-/* Открыть попап по кнопке редактировать */
-redactButton.addEventListener('click', popupToggle);
 
-/* Закрыть попап на крестик */
-closeButton.addEventListener('click', popupToggle);
-
+/* Форма профиля пользователя */
+/* Открыть попап формы профиля пользователя по кнопке редактировать */
+redactButton.addEventListener('click',  () => popupToggle(profileFormPopup));
+/* Закрыть попап формы профиля пользователя на крестик */
+closeProfileButton.addEventListener('click', () => popupToggle(profileFormPopup));
 /* Отправка формы */
-form.addEventListener('submit', formSubmitHandler);
+profileForm.addEventListener('submit', formSubmitProfile);
 
-/* */
+
+/* Попап формы добавления места */
+/* Открыть попап формы добавления места */
+addPlaceButton.addEventListener('click',  () => popupToggle(placeFormPopup));
+/* Закрыть попап формы добавления места на крестик */
+closePlaceButton.addEventListener('click',  () => popupToggle(placeFormPopup));
+/* Отправка формы */
+placeForm.addEventListener('submit', formSubmitPlace);
+/* Попап фотографии */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
