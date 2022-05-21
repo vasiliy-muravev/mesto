5992784f304e010e01658c7024491cff02a81fc2
Vasily Muraviev <muravievvv@develop.mos.ru>
2022-05-21 12:54:56 +0300

    added class Section

---
+++ scripts/Section.js
@@ -0,0 +1,23 @@
+export class Section {
+    constructor({ items, renderer }, containerSelector) {
+        this._renderedItems = items;
+        this._renderer = renderer;
+        this._container = document.querySelector(containerSelector);
+    }
+
+    _clear() {
+        this._container.innerHTML = '';
+    }
+
+    _renderer(element) {
+        this._container.append(element);
+    }
+
+    renderItems() {
+        this._clear();
+
+        this._renderedItems.forEach(item => {
+            this._renderer(item);
+        });
+    }
+}
